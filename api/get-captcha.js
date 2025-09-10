13801c2e5d9ad489976232a7e17cac3c41b94102
boykod92 <149164699+boykod92@users.noreply.github.com>
2025-09-10 15:09:32 +0300

    Update get-captcha.js

--- api/get-captcha.js
+++ api/get-captcha.js
@@ -1,7 +1,7 @@
 // api/get-captcha.js
 export default function handler(req, res) {
   res.status(200).json({
-    imageUrl: "https://captcha-server-snowy.vercel.app/captcha.png", // заменишь на свой путь
-    redirectUrl: "https://example.com", // ссылка, куда клик ведёт
+    imageUrl: "https://github.com/boykod92/captcha-server/blob/main/20b509d9-4e79-4f24-abef-3e5d3a8a31bc.png", // заменишь на свой путь
+    redirectUrl: "https://captcha11.tilda.ws/", // ссылка, куда клик ведёт
   });
 }
