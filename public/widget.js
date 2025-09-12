5416af520688c7e1fcb86b410f08d1670ccbd723
boykod92 <149164699+boykod92@users.noreply.github.com>
2025-09-12 18:07:46 +0300

    Update widget.js

--- public/widget.js
+++ public/widget.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Картинка капчи
   const img = document.createElement('img');
-  img.src = 'https://i.ibb.co/0pQK0Xf0/1-6.png'; // твоя картинка
+  img.src = 'https://i.imgur.com/tO4m7Mu.png'; // твоя картинка
   img.style.position = 'absolute';
   img.style.width = '200px';
   img.style.height = '200px';
